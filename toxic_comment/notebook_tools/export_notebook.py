9836eb336ed09b2e87149831fbbf4c35f34de56e
mike-a-yen <mike.a.yen@gmail.com>
2019-09-08 12:25:14 -0700

    WIP, data generaor

--- toxic_comment/notebook_tools/export_notebook.py
+++ toxic_comment/notebook_tools/export_notebook.py
@@ -21,7 +21,7 @@ def get_export_cells(notebook_json):
     for cell in notebook_json['cells']:
         cell_type = cell['cell_type']
         source_code = get_cell_code(cell)
-        if cell_type == 'code' and source_code.startswith('#export\n'):
+        if cell_type == 'code' and source_code.startswith('#export'):
             export_cells.append(cell)
     return export_cells
 
@@ -33,13 +33,46 @@ def get_export_filename(notebook_path: Path) -> Path:
     return export_path
 
 
+def get_cell_path(cell):
+    first_line = cell['source'][0]
+    if first_line.strip().endswith('.py'):
+        ending = first_line.split()[1].strip()
+        return PROJECT_DIRNAME / f'exports/{ending}'
+    return None
+
+
+def accumulate_cells(cells) -> dict:
+    where_to_save = dict()
+    for cell in cells:
+        save_path = get_cell_path(cell)
+        where_to_save[save_path] = where_to_save.get(save_path, []) + [cell]
+    return where_to_save
+
+
 def save_cells(cells, path):
+    save_data = accumulate_cells(cells)
+    save_data[str(path)] = save_data.pop(None,[])
+    for path, cells in save_data.items():
+        with open(path, 'w') as fw:
+            code = '\n'.join([get_cell_code(cell) for cell in cells])
+            fw.write(code)
+    return
+
     print(f'Saving {len(cells)} cells to {path}')
-    with open(path, 'w') as fw:
-        for cell in cells:
-            code = get_cell_code(cell)
+    for cell in cells:
+        code = get_cell_code(cell)
+        save_path = get_cell_path(cell)
+        if save_path is None: save_path = path
+        print(save_path)
+        with open(path, 'w+') as fw:
             fw.write(code)
             fw.write('\n')
+        
+    #with open(path, 'w') as fw:
+    #    for cell in cells:
+    #        code = get_cell_code(cell)
+    #        fw.write(code)
+    #        fw.write('\n')
     return
 
 def main(path: str) -> None:
--- toxic_comment/train.py
+++ toxic_comment/train.py
@@ -1,8 +1,15 @@
+import fire
 from pathlib import Path
+import string
+
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 from exports.exp_00 import RawDataset, ProcessedDataset, TrainableDataset
+from exports.vocab import Vocabulary, VocabEncoder
 
 PROJECT_DIRNAME = Path('./').resolve()
+
 
 def load_dataset(project_path: Path, subsample: int = 0):
     raw_dataset = RawDataset(project_path, subsample=subsample)
@@ -10,7 +17,33 @@ def load_dataset(project_path: Path, subsample: int = 0):
     trainable_dataset = TrainableDataset(processed_dataset)
     return trainable_dataset
 
+class DataGenerator(Sequence):
+    def __init__(self, df,  transform=None, batch_size: int = 32):
+        self.df = df
+        self.transform = transform
+        self.batch_size = batch_size
+        self.target = 'toxic'
+
+    def __len__(self) -> int:
+        return int(np.ceil(self.df.shape[0]/self.batch_size))
+
+    def __getitem__(self, idx: int):
+        batch_df = self._get_batch(idx)
+        X = batch_df.comment_words
+        X = self.transform(X)
+        Y = batch_df[self.target]
+        return X, Y
+def main(subsample: int = 0) -> None:
+    trainable_dataset = load_dataset(PROJECT_DIRNAME, subsample)
+    vocab = Vocabulary(list(string.printable))
+    token_encoder = VocabEncoder(vocab)
+    
+    X_col = 'comment_words'
+    X_train = trainable_dataset.train_df[X_col].apply(token_encoder.encode)
+    print(X_train)
+    return
+
+
 if __name__ == '__main__':
-    print(PROJECT_DIRNAME)
-    load_dataset(PROJECT_DIRNAME, 100)
+    fire.Fire(main)
     print('Done.')
