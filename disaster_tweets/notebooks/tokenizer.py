4af0206f47d67372a4e343bdd88df116bac8ef4c
mike-a-yen <mike.a.yen@gmail.com>
2020-01-19 21:28:16 -0800

    tweet tokenizer, nltk comes with a tweet tokenizer

--- disaster_tweets/notebooks/tokenizer.py
+++ disaster_tweets/notebooks/tokenizer.py
@@ -1,9 +1,14 @@
 from typing import List
 
+from nltk.tokenize import TweetTokenizer
 from nn_toolkit.tokenizer import SimpleTokenizer
 
 
-class TweetTokenizer(SimpleTokenizer):
+class ProjectTokenizer(SimpleTokenizer):
+    def __init__(self) -> None:
+        super().__init__()
+        self.tokenizer = TweetTokenizer(strip_handles=False).tokenize
+
     def postprocessing(self, tokens: List[str]) -> List[str]:
         tokens = [self.clean_token(t) for t in tokens]
         return tokens
@@ -11,4 +16,6 @@ class TweetTokenizer(SimpleTokenizer):
     def clean_token(self, token: str) -> str:
         if token.startswith('//t.co'):
             return '//t.co'
+        elif token.startswith('@'):
+            return '<@user-handle>'
         return token
